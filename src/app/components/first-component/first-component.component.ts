72300cf2fcaf6304cd87eeb19e45c0bd8fe32994
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-11-18 21:20:31 -0300

    Colocando nome e idade

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Leonardo';
+  age: number = 30;
 
   constructor() { }
 
